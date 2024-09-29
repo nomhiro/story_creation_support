99f383841e7ebea4fa5342d0e0c3309a3b32a2ab
Hiroki Nomura <nomhiro1204@gmail.com>
2024-09-29 21:00:13 +0900

    Refactor NotFound component to adjust top margin of 404 heading and improve background image styling

--- front/src/app/not-found.tsx
+++ front/src/app/not-found.tsx
@@ -6,9 +6,9 @@ const NotFound = () => {
       className="h-screen flex flex-col justify-center items-center bg-slate-50 text-gray-900"
       style={{ backgroundImage: "url('/MIU404.jpg')", backgroundSize: 'auto', backgroundPosition: 'center' }}
     >
-      <h1 className="text-8xl font-bold" style={{ marginTop: '-50%' }}>404</h1>
+      <h1 className="text-8xl font-bold" style={{ marginTop: '-90%' }}>404</h1>
       <p className="text-4xl font-medium">Not Found</p>
-      <Link href="/" className="mt-4 text-xl text-blue-600 hover:underline">
+      <Link href="/" className="mt-2 text-xl text-blue-600 hover:underline">
         Go Back Page
       </Link>
     </div>
